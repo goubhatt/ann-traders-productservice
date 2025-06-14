454b2b7ca57b234bd9907bd2a25605ef5c686bc7
Goutam Bhattacharya <goubhatt@damco.com>
2025-06-14 11:21:25 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/anntraders/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anntraders.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
