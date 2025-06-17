90e1f4433718d590566bdc7661eecbe9e94e17bf
Goutam Bhattacharya <goubhatt@damco.com>
2025-06-17 11:46:09 +0530

    Changed CORS config to allow OPTIONS

--- src/main/java/com/anntraders/productservice/config/CorsConfig.java
+++ src/main/java/com/anntraders/productservice/config/CorsConfig.java
@@ -29,21 +29,20 @@ public class CorsConfig {
 
     @Bean
     public Filter corsPreflightFilter() {
-        return new Filter() {
-            @Override
-            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-                    throws IOException, ServletException {
+        return (request, response, chain) -> {
+            HttpServletRequest req = (HttpServletRequest) request;
+            HttpServletResponse res = (HttpServletResponse) response;
 
-                HttpServletRequest req = (HttpServletRequest) request;
-                HttpServletResponse res = (HttpServletResponse) response;
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Headers", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 
-                if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-                    res.setStatus(HttpServletResponse.SC_OK);
-                    return;
-                }
-
-                chain.doFilter(request, response);
+            if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+                res.setStatus(HttpServletResponse.SC_OK);
+                return;
             }
+
+            chain.doFilter(request, response);
         };
     }
 }
